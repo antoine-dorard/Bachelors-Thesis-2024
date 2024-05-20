8d1014c6f5fb480bf971181adcaf98d569aef7ee
Antoine Dorard <antoinedorard@gmail.com>
2024-05-20 22:55:42 +0200

    method for parsing method calls using LLM + extract_and_summarize update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import logging
 from mobsfscan.mobsfscan import MobSFScan
 import json
 import os
+import openai
 from openai import OpenAI
 from datetime import datetime as dt
 import argparse
@@ -14,17 +16,69 @@ def scan(source_folder):
     scanner = MobSFScan([source_folder], json=True)
     return scanner.scan()
 
-def summarize(source_folder):
 
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+def parse_method_calls_LLM(method_body, openai_client, model="gpt-3.5-turbo"):
+    system_message = """
+You are a Senior Java Developer and understand the language like no one.  You are given a series of Java lines of code altogether constituting a Java method.
+
+Your role is to reply with any method call that you detect within those lines. 
+Format your replies as follows and do never write something else than this. You may however write more than one method call if you read more than one on one line. 
+(ClassName) instanceName.methodName
+
+Note: there might be no method calls on a line, in which case you reply None. (Constructor calls or not considered method calls)
+
+Example:
+Input: if(hex.length() == 1) hexString.append('0');
+Output: 
+hex.length
+hexString.append
+    """
+    
+    code = method_body.splitlines()
+    calls = []
+    messages = [
+        {"role": "system", "content": system_message}
+    ]
+    
+    
+    for line in code:
+        messages.append({"role": "user", "content": line})
+        
+        response = openai_client.chat.completions.create(
+            model=model,
+            messages=messages
+        )
+        
+        messages.append({"role": "assistant", "content": response.choices[0].message.content})
+        
+        if response.choices[0].message.content != "None":
+            calls.append(response.choices[0].message.content)
+            
+        print("User", line)
+        print("Assistant", response.choices[0].message.content)
+
+    return calls
+
+
+def summarize(code, openai_client, model="gpt-3.5-turbo"):
+    system_message = """
+You are a professional Java code interpreter. Your role is to summarize code while strictly following the two following main rules:
+1. PRECISENESS: Always be as precise as possible. You must include everything relevant.
+2. CONCISENESS: always keep the summary concise, while not breaking the first rule.
+
+You may be given to interpret entire Java files, classes, or only methods.
+If you are given a file, create a few points summary of what that file does, without explaining the details for each method.
+The summary should be a high-level overview of the file's purpose and functionality and not a line-by-line or method by method explanation.
+    """
+    
+    response = openai_client.chat.completions.create(
+        model=model,
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Who won the world series in 2020?"},
-            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-            {"role": "user", "content": "Where was it played?"}
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": code}
         ]
     )
+    return response.choices[0].message.content
     
 def get_method_signature_tostr(node):
     signature = ""
@@ -44,14 +98,20 @@ def get_method_signature_tostr(node):
     
     return signature
 
-# Works only when the file is is formatted correctly (i.e. no syntax errors, and line by line code) 
-def extract_java_methods(java_code):
+def isPositionWithinMethod(mobsf_position, mobsf_lines, method):
+    # TODO Check if the position is within the method. Use this function to summarize only methods that contain vulnerabilities
+    pass
+
+
+def extract_and_summarize(java_code: str, openai_client: str, include_summary=True):
     tree = javalang.parse.parse(java_code)
     methods = []
     class_stack = []
     
-    # obj = {}
-    # obj["classes"] = {}
+    obj = {}
+    obj["classes"] = []
+    
+    java_code = java_code.splitlines()
 
     # Traverse the AST and keep track of class contexts
     for path, node in tree:
@@ -59,61 +119,76 @@ def extract_java_methods(java_code):
             class_stack.append(node.name)
             print()
             print(f"Class: {node.name}")
+            
+            
+            code, end_line, end_col = extract_java_methods_body(java_code, node.position.line, node.position.column)
+            code = "\n".join(code)
+            position = {
+                "start_line": node.position.line,
+                "end_line": end_line,
+                "start_col": node.position.column,
+                "end_col": end_col
+            }
+            
+            summary = ""
+            if include_summary:
+                summary = summarize(code, openai_client)
+                
+            obj["classes"].append({"name": node.name, 
+                                   "position": position, 
+                                   "code": code,
+                                   "summary": summary,
+                                   "methods": []
+                                   })
         elif isinstance(node, javalang.tree.MethodDeclaration):
             current_class = class_stack[-1] if class_stack else None
-            print(get_method_signature_tostr(node))
-            methods.append((current_class, node.position.line, node))
-
-    method_bodies = []
-    lines = java_code.splitlines()
-
-    for current_class, start_line, method in methods:
-        
-        method_code = extract_java_methods_body(lines, start_line, method.position.column)
-        
-        method_bodies.append((current_class, method.name, "\n".join(method_code)))
-
-    return method_bodies
-    
-    
-class Summary:
-    system = """
-        You are a professional Java code interpreter. Your role is to summarize code while strictly following the two following main rules:
-        1. PRECISENESS: Always be as precise as possible. You must include everything relevant.
-        2. CONCISENESS: always keep the summary concise, while not breaking the first rule.
-
-        You may be given to interpret entire Java files, classes, or only methods.
-        If you are given a file, create a few points summary of what that file does, without explaining the details for each method.
-        The summary should be a high-level overview of the file's purpose and functionality and not a line-by-line or method by method explanation.
-    """
-    
-    def __init__(self, api_key):
-        self.client = OpenAI(api_key=api_key)
-        
-    def summarize(self, code, model="gpt-3.5-turbo"):
-        response = self.client.chat.completions.create(
-            model=model,
-            messages=[
-                {"role": "system", "content": self.system},
-                {"role": "user", "content": code}
-            ]
-        )
-        return response.choices[0].message.content
-    
-    def summarize_file(self, file, model="gpt-3.5-turbo"):
-        with open(file, "r") as f:
-            code = f.read()
-        return self.summarize(code, model)
-    
-    def summarize_classes_and_methods(self, file, model="gpt-3.5-turbo"):
-        with open(file, 'r') as f:
-            java_code = f.read()
             
-        methods = extract_java_methods(java_code)
+            code, end_line, end_col = extract_java_methods_body(java_code, node.position.line, node.position.column)
+            code = "\n".join(code)
+            position = {
+                "start_line": node.position.line,
+                "end_line": end_line,
+                "start_col": node.position.column,
+                "end_col": end_col
+            }
+            
+            summary = ""
+            if include_summary:
+                summary = summarize(code, openai_client)
+                
+            if current_class == obj["classes"][-1]["name"]:
+                print(get_method_signature_tostr(node))
+                obj["classes"][-1]["methods"].append({"name": node.name, 
+                                                      "signature": get_method_signature_tostr(node),
+                                                      "position": position, 
+                                                      "code": code,
+                                                      "summary": summary
+                                                      })
+            else:
+                for c in obj["classes"]:
+                    if c["name"] == current_class:
+                        c["methods"].append({"name": node.name, 
+                                             "signature": get_method_signature_tostr(node),
+                                             "position": position, 
+                                             "code": code,
+                                             "summary": summary
+                                             })
+                        break
+                    
+    #         print(get_method_signature_tostr(node))
+    #         methods.append((current_class, node.position.line, node))
+
+    # method_bodies = []
+    # lines = java_code.splitlines()
+
+    # for current_class, start_line, method in methods:
         
-        file_summary = {}
-    
-        return methods
+    #     method_code = extract_java_methods_body(lines, start_line, method.position.column)
+        
+    #     method_bodies.append((current_class, method.name, "\n".join(method_code)))
+
+    # return method_bodies
+    return obj
             
             
 def extract_java_methods_body(lines, start_line, start_col):
@@ -131,9 +206,12 @@ def extract_java_methods_body(lines, start_line, start_col):
                      return type.
 
     Returns:
-    list of str: Contains the lines of code that make up the method's body, including the signature and continuing 
+    tuple containing list of str: Contains the lines of code that make up the method's body, including the signature and continuing 
                  until the closing brace is found.
+    and int: The line number where the method body ends.
     """
+    
+    # TODO Check for semi colon before first opening brackets to skip if interface or abstract method declaration 
     
     current_line_index = start_line - 1  # Adjust index to be zero-based for list access
     signature = lines[current_line_index][start_col - 1:]  # Extract the method signature starting from the given column
@@ -176,55 +254,112 @@ def extract_java_methods_body(lines, start_line, start_col):
             elif lines[current_line_index][current_char] == '}':
                 brace_count -= 1
             
-            current_char += 1
             if brace_count == 0:  # If braces balance out, append and return the method body up to this point
                 method_code.append(lines[current_line_index][:current_char + 1])
-                return method_code
+                return (method_code, current_line_index + 1, current_char + 1)
         
+            current_char += 1
+            
         method_code.append(lines[current_line_index])  # Add the whole line to the method body
         current_line_index += 1
         current_char = 0  # Reset character index for the new line
     
     raise Exception("No closing brace found")
     
-def run():
-    # get api key of LLM model (currently @ openai)
+def run(overriding_dir=None):
+    # get api key of LLM model (currently @ openai) and directory to scan
     parser = argparse.ArgumentParser(description='Scan and summarize Java files for vulnerabilities.')
+    parser.add_argument('--dir', type=str, required=True, help='Directory to scan')
     parser.add_argument('--api-key', type=str, required=True, help='OpenAI API key')
     args = parser.parse_args()
     
+    openai_client = OpenAI(api_key=args.api_key)
+    
+    directory = ""
+    if overriding_dir:
+        directory = overriding_dir
+    else:
+        directory = args.dir
     
     print(f"[{dt.now().strftime('%H:%M:%S')}] Scanning for vulnerabilities...")
-    scan_result = scan("vulnerableapp")
-    # scan = scan("Report.java")
+    scan_result = scan(args.dir)
+    print(f"[{dt.now().strftime('%H:%M:%S')}] Done.")
     
-    f_output_summary = open("output_summary.json", "a")
-    summary = Summary(api_key=args.api_key)
-    for file in os.listdir("vulnerableapp"): # only scans the base directory
-        print(f"[{dt.now().strftime('%H:%M:%S')}] Summarizing file {file}...")
+    obj = []
+    for file in os.listdir(directory): # only scans the base directory
+        full_path = os.path.join(directory, file)
         if file.endswith(".java"):
-            summary_content = summary.summarize_file(os.path.join("vulnerableapp", file))
-            print(summary_content)
-            print("\n\n")
+            print()
+            print(f"[{dt.now().strftime('%H:%M:%S')}] Processing file {full_path}...")
             
-            f_output_summary.write("File: " + file + "\n")
-            f_output_summary.write(summary_content + "\n")
-            f_output_summary.write("\n\n")
+            with open(full_path, "r") as f:
+                java_code = f.read()
+            
+            current_obj = extract_and_summarize(java_code, openai_client, include_summary=False)
     
-    with open("output_scan.json", "w") as f:
+            obj.append({
+                "file": full_path,
+                "code": java_code,
+                "classes": current_obj["classes"],
+            })
+    
+    with open("output_obj.json", "w") as f:
         f.write(json.dumps(scan_result))
+        
+    with open("output_object.json", "w") as f:
+        f.write(json.dumps(obj))
 
 
 if __name__ == '__main__':
-    # run()
+    # run(overriding_dir="vulnerableapp")
+    openai._utils._logs.logger.setLevel(logging.WARNING)
+    openai._utils._logs.httpx_logger.setLevel(logging.WARNING)
     
     parser = argparse.ArgumentParser(description='Scan and summarize Java files for vulnerabilities.')
+    parser.add_argument('--dir', type=str, required=True, help='Directory to scan')
     parser.add_argument('--api-key', type=str, required=True, help='OpenAI API key')
     args = parser.parse_args()
     
-    summary = Summary(api_key=args.api_key)
-    # summary.summarize_classes_and_methods("vulnerableapp/DBManager.java")
-          
-    tests.test_extract_java_methods()
+    method_code = """
+private String computeHash(byte[] data) throws NoSuchAlgorithmException {
+    MessageDigest digest = MessageDigest.getInstance("SHA-1");
+    byte[] encodedhash = digest.digest(data);
+    StringBuilder hexString = new StringBuilder(2 * encodedhash.length);
+    for (byte b : encodedhash) {
+        String hex = Integer.toHexString(0xff & b);
+        if(hex.length() == 1) hexString.append('0');
+        hexString.append(hex);
+    }
+    return hexString.toString();
+}
+    """
+    
+    calls = parse_method_calls_LLM(method_code, OpenAI(api_key=args.api_key))
+    
+    for call in calls:
+        print(call)
+    
+    # parser = argparse.ArgumentParser(description='Scan and summarize Java files for vulnerabilities.')
+    # parser.add_argument('--api-key', type=str, required=True, help='OpenAI API key')
+    # args = parser.parse_args()
+    
+    # openai_client = OpenAI(api_key=args.api_key)
+    
+    # with open("vulnerableapp/DBManager.java", 'r') as f:
+    #     java_code = f.read()
+        
+    # file_obj = extract_and_summarize(java_code, openai_client, include_summary=False)
+    # print(file_obj) 
+    # obj = []
+    # obj.append({
+    #     "file": "vulnerableapp/DBManager.java",
+    #     "code": java_code,
+    #     "classes": file_obj["classes"],
+    # })
+    
+    # with open("output_object.json", "w") as f:
+    #     f.write(json.dumps(obj))
+        
+    #tests.test_extract_java_methods()
     
     
